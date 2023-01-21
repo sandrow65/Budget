59efce5ad601a6d67d9c7d8c9152895f883417ac
sandrow65 <sandrow65@gmail.com>
2023-01-21 21:44:43 +0100

    migration to mysql + containerisation db et app

--- build_run.sh
+++
@@ -1,5 +0,0 @@
-docker build -t budget-image .
-
-docker run --name budget-app -v D:\Budget\Appli\db:/usr/src/app/db -v D:\Budget\Appli\db_users:/usr/src/app/db_users -p 5000:5000 budget-image
-
-docker run --name budgetLouison-app -v D:\Budget\Appli\dbLouison:/usr/src/app/db -v D:\Budget\Appli\db_users:/usr/src/app/db_users -p 5001:5000 budget-image
--- config.py
+++
@@ -1,2 +0,0 @@
-PATH_TO_DB = "/usr/src/app/db/Budget.db"
-PATH_TO_USERS_DB = "/usr/src/app/db_users/Users.sqlite"
